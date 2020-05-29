eb8dec2fb372d226fd38fcb8d0d88ccdf7cc5189
a1942855306 <59405557+a1942855306@users.noreply.github.com>
2020-05-29 14:28:34 +0800

    Update index.vue

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -8,7 +8,7 @@
         <div class="header-container">
     <div>
         <i class="el-icon-s-fold" @click="isCollapse=!isCollapse"> </i>
-        江苏传智播客有限公司
+        后台管理系统
     </div>
     <el-dropdown>
         <div class="avatar-wrap">
